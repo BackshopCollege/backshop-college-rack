e0b8941f4a693ba7acb43dcd0ef69e534fb212e7
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-08-03 23:18:49 +0100

    Sample1 test success

--- racking/samples/sample_1.rb
+++ racking/samples/sample_1.rb
@@ -1,2 +1,15 @@
 # Your RACK App should return HTML PAGE with your name and current time.
 # The web server should be thin
+
+require 'date'
+
+class Sample1App
+  def initialize(name)
+    @name = name
+  end
+
+  def call(env)
+    [200, {"Content-Type" => "text/html"}, ["<html><body><p>#{@name}</p><br><p>#{DateTime.now.to_s}</p></body></html>"] ]
+  end
+
+end
