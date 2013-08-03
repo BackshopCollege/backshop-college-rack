e072dabe1409985b271724d92f1a4230dca810a9
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2013-08-03 22:17:54 +0100

    app by method

--- racking/cli-commands.rb
+++ racking/cli-commands.rb
@@ -1,17 +1,25 @@
+
 $ irb -rrack
 
   Rack::Handler.constants
   => [:CGI, :FastCGI, :Mongrel, :EventedMongrel, :SwiftipliedMongrel, :WEBrick, :LSWS, :SCGI, :Thin]
+ 
+  # 1
   app = lambda {|env| [200, {"Content-Type" => "text/plain" } , ["Be welcome"]  ]}
   Rack::Handler::Mongrel.run app, :Port => 3000
   # Go to your browser and point to http://localhost:3000
   # BAMN
+
 --
+
+  # 2
   Rack::Handler::WEBrick.run app, :Port => 3000
   # Go to your browser and point to http://localhost:3000
   # BAMN
 
 --   
+  # 3
+
   class Backshop
     def call(env)
       [200, {"Content-Type" => "text/plain" } , ["Be welcome"] ]
@@ -25,7 +33,7 @@ $ irb -rrack
 -- 
   # Add thin to your Gemfile
   # bundle install
-  
+  # 4
   class Backshop
     def call(env)
       [200, {"Content-Type" => "text/plain" } , ["Be welcome Thin"] ]
@@ -36,6 +44,16 @@ $ irb -rrack
   # Go to your browser and point to http://localhost:3000
   # BAMN
 --
+  # 5
+  class Backshop
+    def call(env)
+      [200, {"Content-Type" => "text/plain" } , ["Be welcome Thin"] ]
+    end
+  end
+
+  Rack::Handler::Thin.run Backshop.new.method(:call), :Port => 3000
+
+
 
 
 
